5ee02f2d3d7aaff5dedaccc5879c4436ed484b62
Juan Martinez <mcplux@outlook.com>
2026-03-26 21:04:47 -0500

    Python + Django + DRF: Add register endpoint

--- python-django-drf/apps/accounts/views.py
+++ python-django-drf/apps/accounts/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
 
-# Create your views here.
+from .serializers import RegisterSerializer
+
+
+class RegisterView(CreateAPIView):
+    serializer_class = RegisterSerializer
